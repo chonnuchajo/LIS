fb9edff57e8b4f74ca035f0a4acfa8530e94395a
prompiriya-ICP <prompiriya.t.icpladda@gmail.com>
2026-07-03 16:13:58 +0700

    fix error

--- server/routes/petitions.js
+++ server/routes/petitions.js
@@ -1,4 +1,4 @@
-sconst express = require('express');
+const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Petition = require('../models/Petition');
