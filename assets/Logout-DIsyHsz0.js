fa96832ca9ce4c5a511f7456ea83b27536703a33
Ilada-Tps <ilada.t@icpladda.com>
2026-07-31 15:09:19 +0700

    Merge branch 'develop' of https://github.com/chonnuchajo/LIS

--- assets/Logout-DIsyHsz0.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:assets/Logout-DIsyHsz0.js
-import{r as o}from"./vendor-react-BrcJiHE1.js";import{u as r}from"./main-BYWpvWwl.js";import"./vendor-msal-pq7o4QqD.js";import"./vendor-query-B9oVrpGH.js";function m(){const{logout:t}=r();return o.useEffect(()=>{t()},[t]),null}export{m as default};
-========
-import{r as o}from"./vendor-react-BrcJiHE1.js";import{u as r}from"./main-DbRMW8SE.js";import"./vendor-msal-pq7o4QqD.js";import"./vendor-query-B9oVrpGH.js";function m(){const{logout:t}=r();return o.useEffect(()=>{t()},[t]),null}export{m as default};
->>>>>>>> 3ac2acd18c6287033f961c336c3282630c8c0468:assets/Logout-Ez1yp-pR.js
--- assets/assignment-DlXVaqh5.js
+++
@@ -1,1 +0,0 @@
-function n(e,m){return!e||!m?!1:m.employeeId&&e.employeeId?e.employeeId===m.employeeId:!!(m.name&&e.name&&e.name===m.name)}export{n as i};
